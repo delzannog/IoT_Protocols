f98beded74c33b37c15561b4b7f6bfa75af24660
Giorgio Delzanno <giorgiodelzanno@gmac-3.local>
2026-04-15 22:10:18 +0200

    Remove folder from repo

--- PROGETTI/FIRPO/assignemnt_protocols_gd/mqtt-subscriber.js
+++
@@ -1,7 +0,0 @@
-var mqttHandler = require('./MqttHandler');
-
-var mqttClient = new mqttHandler('ws://localhost:8080');
-mqttClient.connect();
-
-mqttClient.subscribe((process.argv[2] || 'library'))
-
